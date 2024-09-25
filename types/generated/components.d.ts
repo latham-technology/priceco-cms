f1cfda2ba5cb8ebddbbbabebf94d691ca3dc56fa
Matt Latham <github@mattlatham.dev>
2024-09-24 21:59:23 -0700

    adds ad.singlePage component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
-import type { Schema, Attribute } from '@strapi/strapi'
+import type { Struct, Schema } from '@strapi/strapi'
 
-declare module '@strapi/types' {
-    export module Shared {}
+export interface AdSinglePage extends Struct.ComponentSchema {
+    collectionName: 'components_ad_single_pages'
+    info: {
+        displayName: 'singlePage'
+        icon: 'file'
+    }
+    attributes: {
+        title: Schema.Attribute.String
+        image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required
+    }
+}
+
+declare module '@strapi/strapi' {
+    export module Public {
+        export interface ComponentSchemas {
+            'ad.single-page': AdSinglePage
+        }
+    }
 }
